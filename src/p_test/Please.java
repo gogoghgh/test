ce5d0e3cf4058ca49e62d9889558266e0be20dd2
ygh65 <ygh65@192.168.10.120>
2022-06-06 18:53:50 +0900

    oh yeah

--- src/p_test/Please.java
+++ src/p_test/Please.java
@@ -3,7 +3,9 @@ package p_test;
 public class Please {
 
 	public static void main(String[] args) {
-		System.out.println("Please.. come on..");
+		System.out.println("Please.. come on.. oh yeah!!");
+		//주석 다는 중 ㅎㅎ 앗싸 인제 좀 알겠다
+		
 	}
 
 }
